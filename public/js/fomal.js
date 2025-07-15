86aff80d15cb522716051945e3b76c665d00a1aa
wangzc2012 <1221613@qq.com>
2025-07-15 13:09:16 +0800

    first commit

--- public/js/fomal.js
+++ public/js/fomal.js
@@ -1283,7 +1283,7 @@ function share_() {
     // 截取标题
     var title = document.title;
     var subTitle = title.endsWith("| Fomalhaut🥝") ? title.substring(0, title.length - 14) : title;
-    navigator.clipboard.writeText('Fomalhaut🥝的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
+    navigator.clipboard.writeText('Wangzc2012的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
     new Vue({
       data: function () {
         this.$notify({
